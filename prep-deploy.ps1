492a1c08327764ba37d57fa28f02dfecef22368e
cornelia <cornelia.pelech@gmail.com>
2020-08-09 18:01:22 +0200

    ugly fix for double folder in bundle.js

--- prep-deploy.ps1
+++ prep-deploy.ps1
@@ -1,2 +1,3 @@
 (Get-Content c:/Users/Public/Projects/pnp-puzzles/public/index.html).replace('/build', '/pnp-puzzles/build') | Set-Content c:/Users/Public/Projects/pnp-puzzles/public/index.html
 (Get-Content c:/Users/Public/Projects/pnp-puzzles/public/index.html).replace('/global', '/pnp-puzzles/global') | Set-Content c:/Users/Public/Projects/pnp-puzzles/public/index.html
+(Get-Content c:/Users/Public/Projects/pnp-puzzles/public/build/bundle.js).replace('pnp-puzzles/pnp-puzzles', 'pnp-puzzles') | Set-Content c:/Users/Public/Projects/pnp-puzzles/public/build/bundle.js
