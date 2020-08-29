8fef6c8a1236166192df8af24de0d6e3d4c3dccf
cornelia <cornelia.pelech@gmail.com>
2020-08-29 06:02:38 +0200

    adds gm cheatsheet

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import Menu from './Menu.svelte';
 import Honnleath from './puzzles/Honnleath.svelte';
 import Anagram from './puzzles/Anagram.svelte';
 import Valves from './puzzles/Valves.svelte';
+import GmCheatSheet from './GmCheatSheet.svelte';
 
 const routes = {
   "/": Menu,
   "/Honnleath": Honnleath,
   "/anagram": Anagram,
-  "/valves": Valves
+  "/valves": Valves,
+  "/gm": GmCheatSheet
 };
 
 export default routes;
